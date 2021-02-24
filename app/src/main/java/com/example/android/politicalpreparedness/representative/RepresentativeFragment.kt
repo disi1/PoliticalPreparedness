66f5a16ffa1a4510ce40387f8b67e2d939c4e51a
Diana Sica <diana.sica29@gmail.com>
2021-02-24 17:04:34 +0100

    Representatives screen UI + RecyclerView mechanism for the list of representatives

--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
@@ -7,7 +7,10 @@ import android.os.Bundle
 import android.view.*
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.android.politicalpreparedness.databinding.FragmentRepresentativeBinding
 import com.example.android.politicalpreparedness.network.models.Address
+import com.example.android.politicalpreparedness.representative.adapter.RepresentativeListAdapter
 import java.util.Locale
 
 class DetailFragment : Fragment() {
@@ -16,22 +19,21 @@ class DetailFragment : Fragment() {
         //TODO: Add Constant for Location request
     }
 
-    //TODO: Declare ViewModel
+    private val representativeViewModel by viewModels<RepresentativeViewModel>()
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        //TODO: Establish bindings
+        val binding = FragmentRepresentativeBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = representativeViewModel
 
-        //TODO: Define and assign Representative adapter
-
-        //TODO: Populate Representative adapter
+        binding.representativesRecycler.adapter = RepresentativeListAdapter()
 
         //TODO: Establish button listeners for field and location search
 
-        // just to make it compile
-        return null
+        return binding.root
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
